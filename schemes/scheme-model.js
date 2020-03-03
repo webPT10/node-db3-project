3665557cc34379d5400eaface62c5798e8e13d97
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-03 15:07:10 -0800

    created data/config.js file, imported into scheme-model

---
+++ schemes/scheme-model.js
@@ -0,0 +1,24 @@
+const db = require('../data/')
+
+function find(){
+    return 
+}
+
+function findById(id){}
+
+function findSteps(id){}
+
+function add(scheme){}
+
+function update(changes, id){}
+
+function remove(id){}
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
